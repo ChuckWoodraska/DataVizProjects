0d9b7b94f5e48db3400f5ef294e4fccb45209271
Chuck Woodraska <chuck.woodraska@gmail.com>
2017-07-31 16:34:45 -0400

    Sample pic data and script to grab data. - CW

--- ChuckAndPeople/scripts/grab_pics.py
+++ ChuckAndPeople/scripts/grab_pics.py
@@ -1,8 +1,7 @@
 from instagram.client import InstagramAPI
+import requests
+import json
 
-access_token = "YOUR_ACCESS_TOKEN"
-client_secret = "YOUR_CLIENT_SECRET"
-api = InstagramAPI(access_token=access_token, client_secret=client_secret)
-recent_media, next_ = api.user_recent_media(user_id="userid", count=10)
-for media in recent_media:
-    print(media.caption.text)
+access_token = ""
+response = requests.get('https://api.instagram.com/v1/users/self/media/recent/?access_token={}'.format(access_token))
+print(response.text)
