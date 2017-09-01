bc80b4ea77ae76756800f54318199c3b87d50286
Chuck Woodraska <chuck.woodraska@gmail.com>
2017-09-01 15:21:49 -0400

    Instagram API is dumb and will only do the most recent 20. - CW

--- ChuckAndPeople/scripts/grab_pics.py
+++ ChuckAndPeople/scripts/grab_pics.py
@@ -1,7 +1,18 @@
-from instagram.client import InstagramAPI
 import requests
 import json
 
 access_token = ""
-response = requests.get('https://api.instagram.com/v1/users/self/media/recent/?access_token={}'.format(access_token))
+
+response = requests.get('https://api.instagram.com/v1/users/self/media/recent?access_token={}'.format(access_token))
 print(response.text)
+print(len(json.loads(response.text)['data']))
+for pic_data in json.loads(response.text)['data']:
+    pic_url = pic_data['images']['standard_resolution']['url']
+    p = requests.get(pic_url, stream=True)
+    print(p.url)
+    if p.status_code == 200:
+        with open('../data/pics/{}.jpg'.format(pic_data['created_time']), 'wb') as f:
+            for chunk in p.iter_content(1024):
+                f.write(chunk)
+    print(pic_data['caption']['text'])
+    print(pic_data['images']['standard_resolution']['url'])
