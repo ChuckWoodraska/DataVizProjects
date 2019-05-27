657fe53101a5571989c88045eb49beb2d441d7dd
Chuck Woodraska <chuck.woodraska@gmail.com>
2019-05-27 16:12:31 -0400

    WIP Chuck Rates Beer templates. - CW

--- ChuckRatesBeer/scripts/grab_beer_stats.py
+++ ChuckRatesBeer/scripts/grab_beer_stats.py
@@ -1,6 +1,39 @@
 from ChuckRatesBeer.scripts.untappd import Untappd
 from chuck_pyutils import core as utils
+import requests
+# import pandas as pd
+import urllib.parse as urllib
 
 config = utils.read_config(utils.get_file_path(__file__, 'config.ini'))
-untappd_conn = Untappd(config['UNTAPPD']['CLIENT_ID'], config['UNTAPPD']['CLIENT_SECRET'])
-print(untappd_conn.feed('user', 'chuckwoodraska'))
+# untappd_conn = Untappd(config['UNTAPPD']['CLIENT_ID'], config['UNTAPPD']['CLIENT_SECRET'])
+# a = untappd_conn.feed('user', 'chuckwoodraska').get('response').get('checkins').get('items')
+# print(a)
+
+# api_auth = f"https://untappd.com/v4/search/beer?client_id={config['UNTAPPD']['CLIENT_ID']"
+# result = requests.get(api_auth)
+# print(result)
+# print(result.text)
+
+
+#
+#
+# result = requests.get(API_EP, params=kwargs).json()
+# print(result)
+#
+# while result['response']['pagination']['next_url']:
+#     for x in result['response']['checkins']['items']:
+#         print(x)
+#     result = requests.get(result['response']['pagination']['next_url'], params=kwargs).json()
+#     print(result)
+
+def get_search_beers(query):
+    API_EP = 'https://api.untappd.com/v4/search/beer'
+    kwargs = {"client_id": config['UNTAPPD']['CLIENT_ID'], "client_secret": config['UNTAPPD']['CLIENT_SECRET'],
+              "q": query}
+    result = requests.get(API_EP, params=kwargs).json()
+    for item in result["response"]["beers"]["items"]:
+        print(item["beer"]["bid"], item["beer"]["beer_name"], item["brewery"]["brewery_name"])
+    print()
+
+get_search_beers("Guinness")
+
